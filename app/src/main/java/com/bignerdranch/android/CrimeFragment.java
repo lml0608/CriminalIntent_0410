30b5fced07ef7ea4fd817bdcdaa64f5686e3a3e1
liubin001 <183773928@qq.com>
2017-04-11 17:19:39 +0800

    1.1

--- app/src/main/java/com/bignerdranch/android/CrimeFragment.java
+++ app/src/main/java/com/bignerdranch/android/CrimeFragment.java
@@ -6,6 +6,9 @@ import android.support.v4.app.Fragment;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
@@ -108,5 +111,32 @@ public class CrimeFragment extends Fragment {
         });
 
         return v;
+    }
+
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+
+        inflater.inflate(R.menu.fragment_crime_setting, menu);
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+
+            case R.id.menu_item_delete_crime:
+                //删除当前viewpage显示的crime
+
+
+
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
     }
 }
